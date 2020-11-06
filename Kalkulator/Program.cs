28b8c528b45d6adb494c8ad86122ce317428d8e2
Tera <24725862+teraa@users.noreply.github.com>
2020-11-07 00:37:06 +0100

    add emulator

--- Kalkulator/Program.cs
+++ Kalkulator/Program.cs
@@ -1,9 +1,19 @@
-namespace Kalkulator
+using System;
+
+namespace PrvaDomacaZadaca_Kalkulator
 {
     public class Program
     {
-        static void Main(string[] args)
+    static void Main(string[] args)
+    {
+        var calc = Factory.CreateCalculator();
+        ConsoleKeyInfo key;
+        while (true)
         {
+            Console.WriteLine(calc.GetCurrentDisplayState());
+            key = Console.ReadKey(true);
+            calc.Press(key.KeyChar);
         }
+    }
     }
 }
